5b01aa2279c498155f48479324ad3fb4cabf8e46
Sabmanny <95466606+Abiodun-Sabitu@users.noreply.github.com>
2025-06-14 23:23:40 +0100

    update server.js for pipeops deployment

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ require("dotenv").config();
 const connectDB = require("./src/config/db");
 const app = require("./src/app");
 
+// Connect to the database
 connectDB();
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const isDev = process.env.NODE_ENV === "development";
+
+const HOST = isDev ? "process.env.HOST" : "0.0.0.0";
+
+app.listen(PORT, HOST, () => {
+ console.log(`Server running at http://${HOST}:${PORT}`);
 });
+
+
+
