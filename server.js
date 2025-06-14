97179f5dfecdaa67fc5442652d93c1fc1410223c
Sabmanny <95466606+Abiodun-Sabitu@users.noreply.github.com>
2025-06-14 22:40:00 +0100

    update server.js

--- server.js
+++ server.js
@@ -2,18 +2,10 @@ require("dotenv").config();
 const connectDB = require("./src/config/db");
 const app = require("./src/app");
 
-// Connect to the database
 connectDB();
 
 const PORT = process.env.PORT || 8000;
 
-const isDev = process.env.NODE_ENV === "development";
-
-const HOST = isDev ? "localhost" : "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
- console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-
-
